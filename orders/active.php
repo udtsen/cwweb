09937e6ca068524272eeaa67bd5e6dc56108c31f
Bohdan Zozuliak <z0z1k8k@gmail.com>
2024-06-05 01:00:07 +0300

    courier get method

--- orders/active.php
+++ orders/active.php
@@ -9,21 +9,25 @@ $res = mysqli_query($conn, $sql);
 while ($row = mysqli_fetch_assoc($res)) {
     $places[$row['id']] = $row['name'];
 }
-
-$sql = "SELECT * FROM orders WHERE dt_delivered IS NULL";
+$sql = "SELECT * FROM orders LEFT JOIN users ON orders.courier_id = users.id WHERE dt_delivered IS NULL";
 
 if ($user['status'] == 'place') {
     $id = $user['id'];
-    $sql = "SELECT * FROM orders WHERE user_id = '$id' AND dt_delivered IS NULL";
+    $sql = "SELECT * FROM orders LEFT JOIN users ON orders.courier_id = users.id WHERE user_id = '$id' AND dt_delivered IS NULL";
 }
 
 $res = mysqli_query($conn, $sql);
+
 while ($row = mysqli_fetch_assoc($res)) {
     $orders[] = $row;
 }
 
 foreach ($orders as &$order) {
     $order['place_name'] = $places[$order['place_id']];
+    $order['courier_name'] = "Шукаємо";
+    if ($order['courier_id']) {
+        $order['courier_name'] = $order['first_name'] . " " . $order['last_name'];
+    }
 }
 ?>
 
@@ -37,6 +41,7 @@ foreach ($orders as &$order) {
         <th>Ціна доставки</th>
         <th>Додаткова інформація</th>
         <th>Створено</th>
+        <th>Кур'єр</th>
         <th>Кур'єр отримав о</th>
     </tr>
     <?php foreach($orders as $order): ?>
@@ -48,6 +53,13 @@ foreach ($orders as &$order) {
         <td><?= $order['price'] ?></td>
         <td><?= $order['info'] ?></td>
         <td><?= $order['dt_create'] ?></td>
+        <td>
+            <?= $order['courier_name'] ?>
+            <?php if($order['courier_id'] == NULL && $user['status'] == 'courier') { ?>
+                <a href="/orders/get.php">Взяти</a>
+            <?php } ?>
+
+        </td>
         <td><?= $order['dt_get'] ?></td>
     <?php endforeach; ?>
 </table>
